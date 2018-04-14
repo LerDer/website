3e08af15064ae7276294eef410c7076248fc2aa8
liu <liuweiwei@ll.cc>
2018-04-14 22:57:57 +0800

    用新的对象接收参数，避免恶意参数

--- service/src/main/java/com/nanzhao2018/service/impl/WebUserServiceImpl.java
+++ service/src/main/java/com/nanzhao2018/service/impl/WebUserServiceImpl.java
@@ -6,6 +6,7 @@ import com.nanzhao2018.service.WebUserService;
 import com.nanzhao2018.util.PageResult;
 import com.nanzhao2018.util.QueryObject;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
@@ -18,39 +19,58 @@ import java.util.List;
 @Service
 public class WebUserServiceImpl implements WebUserService {
 	
-	private static final String ERR_1 = "新增失败！";
+	private static final String ERR_1 = "注册失败！";
 	private static final String ERR_2 = "删除失败！";
 	private static final String ERR_3 = "更新失败！";
 	private static final String ERR_4 = "缺少查询参数！";
 	private static final String ERR_5 = "请输入用户名！";
 	private static final String ERR_6 = "请输入密码！";
 	private static final String ERR_7 = "用户名或密码错误，登录失败！";
+	private static final String ERR_8 = "用户名已存在！";
 	
 	
 	@Resource
 	private WebUserMapper webUserMapper;
 	
 	@Override
-	public void addUser(WebUser webUser) {
+	@Transactional(rollbackFor = Exception.class)
+	public void addUser(WebUser user) {
+		WebUser webUser = new WebUser();
+		webUser.setUserName(user.getUserName());
+		webUser.setPassword(user.getPassword());
+		webUser.setMail(user.getMail());
+		webUser.setPhoneNumber(user.getPhoneNumber());
+		webUser.setHeadImg(user.getHeadImg());
+		webUser.setDescription(user.getDescription());
 		int res = webUserMapper.insertUser(webUser);
 		Assert.isTrue(res == 1 , ERR_1);
 	}
 	
 	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public void deleteUser(Long id) {
 		int res = webUserMapper.deletedUserById(id);
 		Assert.isTrue(res == 1 , ERR_2);
 	}
 	
 	@Override
-	public void updateUser(WebUser webUser) {
+	@Transactional(rollbackFor = Exception.class)
+	public void updateUser(WebUser user) {
+		WebUser webUser = new WebUser();
+		webUser.setUserName(user.getUserName());
+		webUser.setPassword(user.getPassword());
+		webUser.setMail(user.getMail());
+		webUser.setPhoneNumber(user.getPhoneNumber());
+		webUser.setHeadImg(user.getHeadImg());
+		webUser.setDescription(user.getDescription());
 		int res = webUserMapper.updateWebUser(webUser);
 		Assert.isTrue(res == 1 , ERR_3);
 	}
 	
 	@Override
-	public WebUser getUser(WebUser webUser) {
-		Assert.isTrue(webUser.getId() != null || webUser.getUserName() != null , ERR_4);
+	public List<WebUser> getUser(WebUser webUser) {
+		Assert.isTrue(webUser.getId() != null || webUser.getUserName() != null ||
+					webUser.getMail() != null || webUser.getPhoneNumber() != null, ERR_4);
 		return webUserMapper.getUserByIdOrName(webUser);
 	}
 	
@@ -66,10 +86,13 @@ public class WebUserServiceImpl implements WebUserService {
 	}
 	
 	@Override
-	public void login(WebUser webUser) {
-		Assert.isTrue(!StringUtils.isEmpty(webUser.getUserName()) , ERR_5);
-		Assert.isTrue(!StringUtils.isEmpty(webUser.getPassword()) , ERR_6);
-		WebUser resUser = webUserMapper.getUserByIdOrName(webUser);
-		Assert.isTrue(resUser != null , ERR_7);
+	public void login(WebUser user) {
+		Assert.isTrue(!StringUtils.isEmpty(user.getUserName()) , ERR_5);
+		Assert.isTrue(!StringUtils.isEmpty(user.getPassword()) , ERR_6);
+		WebUser webUser  =new WebUser();
+		webUser.setUserName(user.getUserName());
+		webUser.setPassword(user.getPassword());
+		List<WebUser> resUsers = webUserMapper.getUserByIdOrName(webUser);
+		Assert.isTrue(resUsers != null && resUsers.size() == 1 , ERR_7);
 	}
 }
