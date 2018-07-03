838863d444f4dec032a0d2d00d86273e9a3aeef3
LerDer <435971036@qq.com>
2018-07-03 22:40:55 +0800

    添加异步,定时,mvc配置注解

--- web/src/main/java/com/nanzhao2018/web/WebConfig.java
+++ web/src/main/java/com/nanzhao2018/web/WebConfig.java
@@ -5,12 +5,18 @@ import com.nanzhao2018.ServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author Ler
  */
 @SpringBootApplication
 @Import({ServiceConfig.class, DaoConfig.class})
+@EnableAsync
+@EnableScheduling
+@EnableWebMvc
 public class WebConfig {
 	public static void main(String[] args) {
 		SpringApplication.run(WebConfig.class , args);
